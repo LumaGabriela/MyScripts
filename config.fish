18f0457ee9ae05cc508c5b90e781f3903db719cd
Luma Gabriela <82393096+LumaGabriela@users.noreply.github.com>
2025-10-14 16:35:34 -0300

    Update config.fish

--- config.fish
+++ config.fish
@@ -6,6 +6,9 @@ ource /usr/share/cachyos-fish-config/cachyos-config.fish
 #    # smth smth
 set -gx nvm_default_version lts
 nvm use default
+
+sdk use java 17.0.16-ms
+
 alias ar 'php artisan'
 #end
 
